1378e331f428316823f24f2e5e0329f53adf6e27
demianchuk-o <demyanchukua@gmail.com>
2023-09-22 15:27:43 +0300

    Created MyQueue class, Implemented MyQueueNode class, implemented default ctor

---
+++ WebNetLab1/WebNetLab1.Collections/MyQueue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+namespace WebNetLab1.Collections;
+
+public class MyQueue<T>
+{
+    private MyQueueNode? _head;
+    private MyQueueNode? _tail;
+
+    public MyQueue()
+    {
+        _head = null;
+        _tail = null;
+    }
+    private class MyQueueNode
+    {
+        public T Data { get; }
+        public MyQueueNode? Next { get; internal set; }
+
+        public MyQueueNode(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
