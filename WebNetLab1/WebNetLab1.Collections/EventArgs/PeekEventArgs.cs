4edf0191d4c0c2d713d9972da2cf1d1bf0b916b7
demianchuk-o <demyanchukua@gmail.com>
2023-09-25 14:24:54 +0300

    Implemented EventArgs for Peek and QueueEmpty events

---
+++ WebNetLab1/WebNetLab1.Collections/EventArgs/PeekEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace WebNetLab1.Collections.EventArgs;
+
+public class PeekEventArgs<T> : System.EventArgs
+{
+    public string Message { get; }
+    public T Data { get; }
+
+    public PeekEventArgs(string message, T data)
+    {
+        Message = message;
+        Data = data;
+    }
+
+}
---
+++ WebNetLab1/WebNetLab1.Collections/EventArgs/QueueEmptyEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace WebNetLab1.Collections.EventArgs;
+
+public class QueueEmptyEventArgs : System.EventArgs
+{ 
+    public string Message { get; }
+
+    public QueueEmptyEventArgs(string message)
+    {
+        Message = message;
+    }
+
+}
