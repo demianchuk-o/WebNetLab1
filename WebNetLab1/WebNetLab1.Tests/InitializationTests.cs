90f94803bdb2f049b1a1f9ab3d0c92d968c06df6
demianchuk-o <demyanchukua@gmail.com>
2023-10-08 20:23:58 +0300

    Created tests for collection initialization

---
+++ WebNetLab1/WebNetLab1.Tests/InitializationTests.cs
@@ -0,0 +1,30 @@
+﻿using WebNetLab1.Collections;
+using Xunit;
+
+namespace WebNetLab1.Tests;
+
+public class InitializationTests
+{
+    [Fact]
+    public void ParameterlessCtor_WhenCalled_ThenEmptyQueue()
+    {
+        var queue = new MyQueue<int>();
+        
+        Assert.Empty(queue);
+    }
+
+    [Fact]
+    public void CtorWithSource_WhenNonEmptySource_ThenQueueEqualsSource()
+    {
+        var source = Enumerable.Range(1, 5);
+        var queue = new MyQueue<int>(source);
+        
+        Assert.Equal(source, queue);
+    }
+
+    [Fact]
+    public void CtorWithSource_WhenEmptySource_ThenThrowArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>((() => { var queue = new MyQueue<int>(null);}));
+    }
+}
