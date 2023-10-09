5d30b57a85e735ace403d4fd998770d6a7d347dc
demianchuk-o <demyanchukua@gmail.com>
2023-10-09 21:10:01 +0300

    Implemented Enqueue tests

---
+++ WebNetLab1/WebNetLab1.Tests/EnqueueTests.cs
@@ -0,0 +1,37 @@
+﻿using WebNetLab1.Collections;
+using WebNetLab1.Tests.ClassData;
+using Xunit;
+
+namespace WebNetLab1.Tests;
+
+public class EnqueueTests
+{
+    
+    [Theory]
+    [ClassData(typeof(MultipleItemsQueueData))]
+    public void Enqueue_WhenEmptyQueue_ShouldAddItems<T>(T[] items)
+    {
+        var queue = new MyQueue<T>();
+
+        foreach (var item in items)
+        {
+            queue.Enqueue(item);
+        }
+        
+        Assert.Equal(items, queue);
+    }
+
+    [Theory]
+    [ClassData(typeof(MultipleItemsQueueData))]
+    public void Enqueue_WhenNonEmptyQueue_ShouldAddItems<T>(T[] items)
+    {
+        var queue = new MyQueue<T>(items);
+
+        foreach (var item in items)
+        {
+            queue.Enqueue(item);
+        }
+        
+        Assert.Equal(items.Length * 2, queue.Count);
+    }
+}
