342436d3f1da0b76e4518577d284807693e5423a
demianchuk-o <demyanchukua@gmail.com>
2023-10-10 13:47:35 +0300

    Implemented analogical TryDequeue tests

--- WebNetLab1/WebNetLab1.Tests/DequeueTests.cs
+++ WebNetLab1/WebNetLab1.Tests/DequeueTests.cs
@@ -50,4 +50,51 @@ public class DequeueTests
 
         Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
     }
+    
+    [Theory]
+    [ClassData(typeof(MultipleItemsQueueData))]
+    public void TryDequeueOne_WhenNonEmptyQueue_ThenRemoveAndReturnTrueAndDecreaseCount<T>(T[] items)
+    {
+        var queue = new MyQueue<T>(items);
+
+        var dequeuedResult = queue.TryDequeue(out var dequeuedItem);
+        
+        Assert.True(dequeuedResult);
+        Assert.Equal(items[0], dequeuedItem);
+        Assert.Equal(items.Length - 1, queue.Count);
+    }
+
+    [Theory]
+    [ClassData(typeof(MultipleItemsQueueData))]
+    public void TryDequeueMany_WhenNonEmptyQueue_ThenRemoveAndReturnTrueAndQueueEmpty<T>(T[] items)
+    {
+        var queue = new MyQueue<T>(items);
+
+        foreach (var item in items)
+        {
+            var dequeuedResult = queue.TryDequeue(out var dequeuedItem);
+            Assert.True(dequeuedResult);
+            Assert.Equal(item, dequeuedItem);
+        }
+        
+        Assert.Empty(queue);
+    }
+    
+    [Fact]
+    public void TryDequeue_WhenEmptyQueue_ThenReturnFalse()
+    {
+        var queue = new MyQueue<int>();
+
+        Assert.False(queue.TryDequeue(out _));
+    }
+    
+    [Theory]
+    [ClassData(typeof(MultipleItemsQueueData))]
+    public void TryDequeue_WhenQueueCleared_ThenReturnFalse<T>(T[] items)
+    {
+        var queue = new MyQueue<T>(items);
+        queue.Clear();
+
+        Assert.False(queue.TryDequeue(out _));
+    }
 }
